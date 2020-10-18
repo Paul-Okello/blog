51fdba874dc7a7f99460510cf8361cdd28fcf011
Paul Okello <paulokello273@gmail.com>
2020-10-18 16:40:54 +0300

    formating

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,7 @@
+import Head from "next/head";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -9,7 +11,8 @@ export async function getStaticPaths() {
   };
 }
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
@@ -19,11 +22,16 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      {<Date dateString={postData.date} />}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
